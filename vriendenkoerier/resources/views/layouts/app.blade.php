d424c4456d172fa028c72efa4b57bb6498e171ae
Mio Spruit <mio12@hotmail.nl>
2019-11-05 15:45:04 +0100

    Change create page.

--- vriendenkoerier/resources/views/layouts/app.blade.php
+++ vriendenkoerier/resources/views/layouts/app.blade.php
@@ -13,13 +13,17 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+        <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Baloo+Bhai|Montserrat&display=swap" rel="stylesheet">
 
-        <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <!-- Styles -->
+
     </head>
+
 
     <body>
         <main>
